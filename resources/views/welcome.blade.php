efeec93f0082f283dc381ee6a18696eee0675c6f
AbhinavVihan <89139362+AbhinavVihan@users.noreply.github.com>
2024-12-26 09:27:12 +0530

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to Laravel</title>
+    <title>Welcome to Laravel and hello there</title>
 </head>
 <body>
     <h1>Welcome to Laravel!</h1>
